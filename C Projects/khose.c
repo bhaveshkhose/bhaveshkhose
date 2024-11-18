2f25335a96e0508656915944bfecbe4424cb738f
bhaveshkhose <khosebhavesh@gmail.com>
2024-11-18 10:43:43 +0530

    Update khose.c

--- C Projects/khose.c
+++ C Projects/khose.c
@@ -1,16 +1,36 @@
 #include<stdio.h>
+//Method for sorting array by Insertion sort
 void insertionSort(int arr[], int n) {
     for (int i = 1; i < n; i++) {
         int key = arr[i];
         int j = i - 1;
 
-        // Move elements of arr[0..i-1] that are greater than key
-        // to one position ahead of their current position
+        
         while (j >= 0 && arr[j] > key) {
             arr[j + 1] = arr[j];
             j--;
         }
         arr[j + 1] = key;
+    }
+}
+//method for sorting array by selection sort
+void selectionSort(int arr[], int n) {
+    int i, j, minIndex, temp;
+
+    // Traverse through all array elements
+    for (i = 0; i < n - 1; i++) {
+        // Find the minimum element in the unsorted part
+        minIndex = i;
+        for (j = i + 1; j < n; j++) {
+            if (arr[j] < arr[minIndex]) {
+                minIndex = j;
+            }
+        }
+
+        // Swap the found minimum element with the first element
+        temp = arr[minIndex];
+        arr[minIndex] = arr[i];
+        arr[i] = temp;
     }
 }
 
